dfda0abdba1ecbed231b94aa8102056aefe05474
vkrmk <vikramkini9@gmail.com>
2023-05-18 19:55:13 +0530

    Initial Commit

---
+++ WardCard Game/WardCard_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardCard_GameApp.swift
+//  WardCard Game
+//
+//  Created by Vikram Kini on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WardCard_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
